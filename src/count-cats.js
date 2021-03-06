46065492de68c1398ce0f8a6249f83146090c559
Anastasia Shrai <nastia.shrai@gmail.com>
2021-03-06 16:59:13 +0200

    fix: add solutions to few tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+  let arr1 = [].concat(...arr);
+  let num = 0;
+  for (let i=0; i<arr1.length; i++) {
+    if (arr1[i] === '^^') 
+      num = num + 1;      
+  } return num;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members) || members === null)
+    return false;
+  members = members.filter(member => {
+   return typeof member === "string"
+  });
+
+for (let i = 0; i < members.length; i++) {
+  if (typeof members[i] === "string") {
+    members[i] = members[i].split(' ').join('').toUpperCase();
+  }
+}
+members = members.sort();
+const dreamTeam = members.map(member => {
+  return member[0];
+});
+return dreamTeam.join('');
 };
